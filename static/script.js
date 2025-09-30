9853069fa39c7d75503af0e982c2187e96babb78
Vinícius Diller <viniciusdiller@hotmail.com>
2025-09-30 02:13:38 -0300

    mrm coisa

--- static/script.js
+++ static/script.js
@@ -131,6 +131,8 @@ function displayResults(data) {
   const translationMap = {
     classification: "Classificação",
     confidence_score: "Nível de Confiança",
+    key_topic: "Tópico Principal", // CORRIGIDO
+    sentiment: "Sentimento",
   };
   const suggestedResponse =
     data.suggested_response || "Nenhuma resposta necessária.";
